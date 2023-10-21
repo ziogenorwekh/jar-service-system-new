2e17a779945a0bb4008d19bb1954baac87faa827
ziogenorwekh <ziogenorwekh@gmail.com>
2023-10-21 16:59:27 +0900

    add docker-compose modify testing bootstrap.yml

--- apporder-service/apporder-container/src/main/java/com/jar/service/system/apporder/service/container/BeanConfiguration.java
+++ apporder-service/apporder-container/src/main/java/com/jar/service/system/apporder/service/container/BeanConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfiguration {
-
     @Bean
     public AppOrderDomainService appOrderDomainService() {
         return new AppOrderDomainServiceImpl();
