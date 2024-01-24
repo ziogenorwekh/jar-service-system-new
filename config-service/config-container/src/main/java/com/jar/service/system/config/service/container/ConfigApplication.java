b77b88002dd3396445b81a56be97efdaee0e8314
ziogenorwekh <ziogenorwekh@gmail.com>
2024-01-24 19:59:25 +0900

    modify config-service

--- config-service/config-container/src/main/java/com/jar/service/system/config/service/container/ConfigApplication.java
+++ config-service/config-container/src/main/java/com/jar/service/system/config/service/container/ConfigApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("/settings.yml")
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
